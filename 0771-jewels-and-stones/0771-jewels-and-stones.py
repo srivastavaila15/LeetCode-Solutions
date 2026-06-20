a639ad28188cab2923e46a31667d8722664f1695
srivastavaila15 <srivastavaila15@gmail.com>
2026-06-19 19:44:55 -0500

    Time: 0 ms (100%), Space: 12.4 MB (18.33%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,12 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
+        jSet = set()
+        for i in range(len(jewels)):
+            jSet.add(jewels[i])
         count = 0
-        for char in stones:
-            if char in jewels:
+        for i in range(len(stones)):
+            if stones[i] in jSet:
                 count = count + 1
         return count
+
         
