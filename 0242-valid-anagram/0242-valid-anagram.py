699de5f1825bec085e29280b2f8b334fe4b42686
srivastavaila15 <srivastavaila15@gmail.com>
2026-07-01 12:02:25 -0500

    Time: 14 ms (89.11%), Space: 12.6 MB (51.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        new_s = "".join(sorted(s))
-        new_t = "".join(sorted(t))
-        if new_s == new_t:
+        s_map = {}
+        t_map = {}
+        for ch in s:
+            if ch not in s_map:
+                s_map[ch] = 1
+            else:
+                s_map[ch] = s_map[ch] + 1
+        for ch in t:
+            if ch not in t_map:
+                t_map[ch] = 1
+            else:
+                t_map[ch] = t_map[ch] + 1
+        if s_map == t_map:
             return True
         return False
+
         
