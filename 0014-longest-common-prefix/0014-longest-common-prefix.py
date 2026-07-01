e4c6353f96277b61b219412a847a99fd526e82fe
srivastavaila15 <srivastavaila15@gmail.com>
2026-07-01 11:45:36 -0500

    Time: 0 ms (100%), Space: 12.4 MB (33.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,10 +3,11 @@ class Solution(object):
         x = 0
         while x < len(strs[0]):
             ch = strs[0][x]
-            for i in range(1, len(strs)):
+            for i in range(len(strs)):
                 if x == len(strs[i]) or ch != strs[i][x]:
                     return strs[0][:x]
             x = x + 1
         return strs[0]
+            
         
         
