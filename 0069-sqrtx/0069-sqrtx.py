626809a3333251bf4b86a6414dfb9dee0b83baa1
srivastavaila15 <srivastavaila15@gmail.com>
2026-07-18 16:40:05 -0500

    Time: 4 ms (42.38%), Space: 12.3 MB (54.17%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,16 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        power = 0.5
-        res = x ** power
-        return int(res)
-        
+class Solution(object):
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+        left = 2
+        right = x/2
+        while left <= right:
+            mid = (left + right)/2
+            if x == mid**2:
+                return mid
+            elif mid**2 < x:
+                left = mid + 1
+            else:
+                right = mid -1
+        return right
         
