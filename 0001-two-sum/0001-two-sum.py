2096f681e0a3f1f464c42fac7e8e9683e5b6748f
srivastavaila15 <srivastavaila15@gmail.com>
2026-07-31 19:17:17 -0500

    Time: 0 ms (100%), Space: 13.3 MB (36.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        pair_map = {}
+        for i in range(0, len(nums)):
+            if nums[i] not in pair_map:
+                pair_map[nums[i]] = i
+        
+        for j in range(0, len(nums)):
+            pair_val = target - nums[j]
+            if pair_val in pair_map and pair_map[pair_val] != j:
+                return [j, pair_map[pair_val]]
         
         
