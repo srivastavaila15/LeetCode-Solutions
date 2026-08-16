26149a02778e013057de3699011b27d7c9d44b51
srivastavaila15 <srivastavaila15@gmail.com>
2026-08-16 16:11:24 -0500

    Time: 0 ms (100%), Space: 12.3 MB (89.14%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        if num == 0: return "0"
+        original_num = num
+        num = abs(num)
+        remainders = []
+        while num > 0:
+            remainder = num % 7
+            remainders.append(str(remainder))
+            num = num // 7
+        if original_num < 0:
+            remainders.append('-')
+        remainders.reverse()
+        return ''.join(remainders)
+
+
+
